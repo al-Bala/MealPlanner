b8a49c3cb8c63c02446bebf8b77c225808aaabba
al-Bala <aleksandra.bala.01@gmail.com>
2023-11-04 17:32:36 +0100

    Initial commit

---
+++ src/test/java/com/mealplanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealplanner.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
