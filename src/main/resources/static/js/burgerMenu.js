91cd727f9efcdd0b5712610a50c15d2764dae7ce
GrucaCode <basiag2001@gmail.com>
2024-01-04 02:47:31 +0100

    user-home stylowanie

--- src/main/resources/static/js/burgerMenu.js
+++ src/main/resources/static/js/burgerMenu.js
@@ -2,10 +2,15 @@
 
 window.addEventListener("DOMContentLoaded", () => {
     const burgerBtn = document.querySelector('.burger-btn');
+    const exitBtn = document.querySelector('.exit-btn');
     const menuItems = document.querySelector('.menu');
+    const navbar = document.querySelector('nav');
+
 
     const showMenu = () => {
         menuItems.classList.toggle('active');
+        navbar.classList.toggle('active');
     }
     burgerBtn.addEventListener('click', showMenu);
+    exitBtn.addEventListener('click', showMenu);
 });
