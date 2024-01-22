d4ed71f5db3c20eea76f6cd0d5f5159f76d368e7
Barbara Gruca <basiag2001@gmail.com>
2024-01-22 02:27:24 +0100

    serduszko i menu ostatnie poprawki gotowe

--- src/main/resources/static/js/favRecipe.js
+++ src/main/resources/static/js/favRecipe.js
@@ -1,16 +1,17 @@
 const emptyHeart = document.querySelector('.fa-regular.fa-heart');
-const solidHeart = document.querySelector('.solid.fa-heart');
+const solidHeart = document.querySelector('.fa-solid.fa-heart');
 const favBtn = document.querySelector('.favourite__btn');
+const trueFav = document.querySelector('.fav-bool');
 
 const addFavourite = () => {
-    if(emptyHeart.style.display === "flex") {
+    if(trueFav.textContent === 'true') {
         solidHeart.style.display = "flex";
         emptyHeart.style.display = "none";
-    } else {
-        solidHeart.style.display = "flex";
-        emptyHeart.style.display = "none";
+    } else if (trueFav.textContent === 'false') {
+        solidHeart.style.display = "none";
+        emptyHeart.style.display = "flex";
     }
     
 }
 
-favBtn.addEventListener('click', addFavourite);
+window.addEventListener('load', addFavourite);
